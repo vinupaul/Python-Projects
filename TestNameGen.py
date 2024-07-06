fc0b69bd1f58a22cbb0584b728b1887cb8734259
Vinu Paul <vinupaul9999@gmail.com>
2024-07-06 12:53:42 -0400

    Create TestNameGen.py

---
+++ TestNameGen.py
@@ -0,0 +1,5 @@
+print("welcome to the random name generator")
+a=input("what is your best friends first name?\n")
+b=input("what do you actually call him?\n")
+
+print("The best suited name could be", a + " " + b)
